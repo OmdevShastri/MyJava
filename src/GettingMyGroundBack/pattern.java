70551bc6745c98685abb05f0095fcac06e3264d3
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2022-01-19 21:27:00 +0530

    19thJan2022

---
+++ src/GettingMyGroundBack/pattern.java
@@ -0,0 +1,35 @@
+package GettingMyGroundBack;
+
+public class pattern {
+    public static void main(String[] args){
+//        System.out.println("*");
+//        System.out.println("**"); //haha, such noob code XD
+//        System.out.println("***");
+        System.out.println("My my");
+
+        //variables
+        String name="wahahaha";
+        int age=10000;
+        double price =23.33;
+        /*
+        Primitive
+        byte-1byte
+        short - 2
+        char - 2
+        boolean - 1
+        int- 4
+        long - 8
+        float - 4
+        double - 8
+
+        Non-Primitive
+        String
+        Array
+        Class
+        Object
+        Interface
+         */
+
+
+    }
+}
