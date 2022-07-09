1d31a818b47aebd67e5115e7894f81d17dd86d94
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2022-07-09 12:31:49 +0530

    newNewnew

---
+++ Practice/Oddnums.java
@@ -0,0 +1,25 @@
+package Practice;
+import java.util.Scanner;
+
+public class Oddnums {
+
+        public static int countOdds(int low, int high) {
+
+//            int low = sc.nextInt();
+//            int high = sc.nextInt();
+//            int count = 0;
+//            for (int i = low; i <= high; i++) {
+//                if (i % 2 != 0) {
+//                    count++;
+//                }
+//            }
+            //return count;
+            return (high+1)/2 - low/2;
+        }
+        public static void main(String args){
+            Scanner sc = new Scanner(System.in);
+            int low = sc.nextInt();
+            int high = sc.nextInt();
+            System.out.println(countOdds(low,high));
+        }
+    }
