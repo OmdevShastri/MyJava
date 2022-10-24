8b10fce3c82d43e49579bcf6184495e835176364
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2022-10-24 23:33:44 +0530

    String functions

---
+++ src/Javapaid/Strings/StringFuncs.java
@@ -0,0 +1,31 @@
+package Javapaid.Strings;
+
+public class StringFuncs {
+    public static String subStr(String str, int si, int ei){
+        String substr = "";
+        for (int i = si; i < ei; i++) {
+            substr+= str.charAt(i);
+        }
+        return substr;
+    }
+    public static void main(String[] args) {
+        String a = "Tony";
+        String b = "Tony";
+        String c = new String("Tony");
+        if (a == c) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("not Equal");
+        }
+        if (a.equals(c)) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("not Equal");
+        }
+
+        System.out.println(subStr("tensile",0,3));
+        String str = "tensile";
+        System.out.println(str.substring(0,3));
+
+    }
+}
