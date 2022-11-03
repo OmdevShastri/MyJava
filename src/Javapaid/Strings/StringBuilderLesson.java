580c541c0e6618f1c1b6bfbe1422c5dd7a9437df
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2022-11-03 23:21:51 +0530

    String builder- toUpper function question

--- src/Javapaid/Strings/StringBuilderLesson.java
+++ src/Javapaid/Strings/StringBuilderLesson.java
@@ -1,12 +1,30 @@
 package Javapaid.Strings;
 
 public class StringBuilderLesson {
+    public static String toUpper(String str){
+        StringBuilder sb = new StringBuilder("");
 
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i)== ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("");
+        String str = new String("hello, i'm your person");
+
         for (char i = 'a'; i <= 'z'; i++) {
             sb.append(i);
         }
         System.out.println(sb);
+        System.out.println(toUpper(str));
     }
 }
