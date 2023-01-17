f7ca4db9c4bc0b039e70679245399ede74b829f6
omdev <omdevsh19ete@student.mes.ac.in>
2023-01-17 09:48:43 +0530

    StringCompress lecture

---
+++ src/Javapaid/Strings/StringCompress.java
@@ -0,0 +1,10 @@
+package Javapaid.Strings;
+public class StringCompress {
+    public static String compress(String str){
+
+        return str;
+    }
+    public static void main(String[] args) {
+
+    }
+}
