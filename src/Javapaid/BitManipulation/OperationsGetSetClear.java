f1d1dc0e6c5f13a4f8f753c22c695570cba713d5
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-02-23 16:18:40 +0530

    added checkPowerOf2, countSetBits

--- src/Javapaid/BitManipulation/OperationsGetSetClear.java
+++ src/Javapaid/BitManipulation/OperationsGetSetClear.java
@@ -33,12 +33,25 @@ public class OperationsGetSetClear {
         int bitmask = (~0)<<i;
         return n & bitmask;
     }
-    public static int clearIBitsInRange(int n, int i, int j){
+    public static int clearIBitsInRange(int n, int i, int j){ //Q1
         int a = ((~0)<<(j+1));
         int b = (1<<i)-1;
         int bitMask = a|b;
 
         return bitMask & n;
+    }
+    public static boolean checkPowerOf2(int n){// Q2
+        return ((n & (n-1))  == 0);
+    }
+    public static int countSetBits(int n){
+    int count =0;
+    while(n >0){
+        if ((n & 1) !=0){
+            count++;
+        }
+        n = n>>1;
+    }
+    return count;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -50,5 +63,7 @@ public class OperationsGetSetClear {
         System.out.println(updateIthBit(7,3,1));
         System.out.println(clearLastIthBits(n, 2));
         System.out.println(clearIBitsInRange(10, 2,4));
+        System.out.println(checkPowerOf2(8));
+        System.out.println(countSetBits(15));
     }
 }
