115868fe9806f98da3154dc85052cdf6d3066510
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-07-27 14:50:10 +0530

    added last occurrence to FirstLastOccurrence

--- src/Javapaid/Recursion/FirstLastOccurrence.java
+++ src/Javapaid/Recursion/FirstLastOccurrence.java
@@ -16,6 +16,9 @@ public class FirstLastOccurrence {
         int key = sc.nextInt();
         System.out.println(occ(arr,key,0));
 
+        System.out.println("Last occurrence at ");
+        System.out.println(occLast(arr,key,n-1));
+
     }
 
     private static int occ(int[] arr, int key, int i) {
@@ -26,5 +29,14 @@ public class FirstLastOccurrence {
             return i;
         }
         return occ(arr, key, i+1);
+    }
+    private static int occLast(int[] arr, int key, int i) {
+        if (i <=0) {
+            return -1;
+        }
+        if (arr[i]==key){
+            return i;
+        }
+        return occ(arr, key, i-1);
     }
 }
