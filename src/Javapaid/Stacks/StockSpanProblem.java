701d3a800936a06a73bf815e84ac702335a17d75
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-09-22 21:51:15 +0530

    Stacks -StockSpanProblem

---
+++ src/Javapaid/Stacks/StockSpanProblem.java
@@ -0,0 +1,35 @@
+package Javapaid.Stacks;
+
+import java.util.Stack;
+
+public class StockSpanProblem {
+    public static void stockSpan(int [] stocks, int [] span){
+        Stack<Integer> s=  new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for (int i = 1; i < stocks.length; i++) {
+            int currP = stocks[i];
+            while (!s.isEmpty() && currP>stocks[s.peek()]){
+                s.pop();
+            }
+            if (s.isEmpty()){
+                span[i]=i+1;
+
+            }else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+
+            }
+            s.push(i);
+        }
+    }
+    public static void main(String[] args) {
+        int [] stocks = {100,80,60,70,60,85,100};
+        int [] span = new int[stocks.length];
+        stockSpan(stocks,span);
+        for (int i = 0; i < span.length; i++) {
+            System.out.print(span[i]+ " ");
+        }
+
+    }
+}
