602205052070f81cbc2351f96b10c0767e15193b
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-09-23 18:49:43 +0530

    reverseQ

---
+++ src/Javapaid/Queues/QueueReversal.java
@@ -0,0 +1,37 @@
+package Javapaid.Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueReversal {
+    public static void reverseQ(Queue<Integer> q){
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()){
+            q.add(s.pop());
+        }
+
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        reverseQ(q);
+        //print
+        while (!q.isEmpty()){
+            System.out.print(q.remove()+ " ");
+        }
+    }
+}
