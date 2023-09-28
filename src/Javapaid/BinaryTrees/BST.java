1e78296ea4baa59c61abc2e9c34ea073dcd2cb6e
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-09-28 15:49:39 +0530

    Binary search tree -sorted array to balanced BST -convert BST to balanced BST -largestBST

--- src/Javapaid/BinaryTrees/BST.java
+++ src/Javapaid/BinaryTrees/BST.java
@@ -1,6 +1,7 @@
 package Javapaid.BinaryTrees;
 
 import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
 
 public class BST {
     static class Node {
@@ -159,16 +160,98 @@ public class BST {
 
     }
 
-    public static void main(String[] args) {
-        int [] vals = {8,5,3,6,10,11};
-        Node root = null;
+    public static Node createBSTArray(int[] arr, int st, int en){
+        if (st>en){
+            return null;
+        }
+        int mid = (st+en)/2;
 
-        for (int i = 0; i < vals.length; i++) {
-            root= insert(root, vals[i]);
+        Node root = new Node(arr[mid]);
+        root.left = createBSTArray(arr, st, mid-1);
+        root.right = createBSTArray(arr, mid+1, en);
+
+        return root;
+    }
+
+    public static void getInOrder(Node root, ArrayList<Integer> inOrder){
+        if (root== null){
+            return;
         }
 
-        inOrderTraverse(root);
-//        if (search(root, 6)){
+        getInOrder(root.left,inOrder);
+        inOrder.add(root.data);
+        getInOrder(root.right,inOrder);
+    }
+
+    public static Node balaceBST(Node root){
+        //calc inOrder sequence
+        ArrayList<Integer> inOrder = new ArrayList<>();
+        getInOrder(root,inOrder);
+
+        //sorted InOrder  to balanced BST
+        root = createBSTArray(inOrder,0,inOrder.size()-1);
+        return root;
+    }
+
+    private static Node createBSTArray(ArrayList<Integer> arr, int st, int en) {
+        if (st>en){
+            return null;
+        }
+        int mid = (st+en)/2;
+
+        Node root = new Node(arr.get(mid));
+        root.left = createBSTArray(arr, st, mid-1);
+        root.right = createBSTArray(arr, mid+1, en);
+
+        return root;
+    }
+
+    public static class Info{
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST, int size, int min, int max) {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static int maxBST =0;
+
+    public static Info lasrgestBST(Node root){
+        if (root == null){
+            return new Info(true, 0, Integer.MAX_VALUE,Integer.MIN_VALUE);
+        }
+        Info leftInfo = lasrgestBST(root.left);
+        Info rightInfo = lasrgestBST(root.right);
+        int size = leftInfo.size + rightInfo.size +1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+        if (root.data<= leftInfo.max|| root.data>=rightInfo.min){
+            return new Info(false, size, min, max);
+        }
+        if (leftInfo.isBST && rightInfo.isBST){
+            maxBST = Math.max(maxBST,size);
+            return new Info(true, size, min, max);
+        }
+        return new Info(false, size, min ,max);
+    }
+
+    public static void main(String[] args) {
+//        int [] vals = {8,5,3,6,10,11};
+//        Node root = null;
+//
+//        for (int i = 0; i < vals.length; i++) {
+//            root= insert(root, vals[i]);
+//        }
+//
+//        inOrderTraverse(root);
+////        if (search(root, 6)){
 //            System.out.println("found");
 //        }else {
 //            System.out.println("not found");
@@ -177,7 +260,7 @@ public class BST {
 //        System.out.println();
 //        prtInRange(root,5,12);
 //        inOrderTraverse(root);
-        System.out.println();
+//        System.out.println();
 //        prtRoot2Leaf(root,new ArrayList<>());
 //        if (isValidBST(root,null,null)){
 //            System.out.println("valid");
@@ -185,9 +268,31 @@ public class BST {
 //            System.out.println(-1);
 //        }
 
-        root = createMirror(root);
-        prtPreOrder(root);
+//        root = createMirror(root);
+//        prtPreOrder(root);
 
+//        int [] arr = {3,5,6,8,10,11,12};
+//
+//        Node root = createBSTArray(arr, 0, arr.length-1);
+//        prtPreOrder(root);
+
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.left.left =new Node(5);
+        root.left.right =new Node(20);
+
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
+
+//        root = balaceBST(root);
+
+//        prtPreOrder(root);
+
+        Info info = lasrgestBST(root);
+        System.out.println(maxBST);
     }
 }
 
