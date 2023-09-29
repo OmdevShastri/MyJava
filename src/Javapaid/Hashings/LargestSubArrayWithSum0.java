03637fb1a16150854f52a72f758582294e33ec53
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-09-29 14:59:54 +0530

    Hashing -LargestSubArrayWithSum0

---
+++ src/Javapaid/Hashings/LargestSubArrayWithSum0.java
@@ -0,0 +1,25 @@
+package Javapaid.Hashings;
+
+import java.util.HashMap;
+
+public class LargestSubArrayWithSum0 {
+    public static void main(String[] args) {
+        int [] arr = {15,-2,2,-8,1,7,10,23};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        //sum, idx
+        int sum =0;
+        int len =0;
+        for (int j = 0; j < arr.length; j++) {
+            sum+= arr[j];
+            if (map.containsKey(sum)){
+                len = Math.max(len, j-map.get(sum));
+            }
+            else {
+                map.put(sum,j);
+            }
+        }
+        System.out.println("Largest "+ len);
+    }
+}
