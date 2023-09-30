f87317eb40bea53a81654ad41b518782e51fe182
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-09-30 16:53:27 +0530

    Trie -LongestWordWithAllPrefixes incomplete

---
+++ src/Javapaid/Tries/LongestWordWithAllPrefixes.java
@@ -0,0 +1,45 @@
+package Javapaid.Tries;
+
+public class LongestWordWithAllPrefixes {
+    static class Node{
+        Node [] children = new Node[26];
+        boolean eow = false;
+        public Node(){
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i)-'a';
+            if (curr.children[idx] == null){
+                curr.children[idx] = new Node();
+
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow =true;
+    }
+
+    public static boolean search(String key){
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i)-'a';
+            if (curr.children[idx]==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
