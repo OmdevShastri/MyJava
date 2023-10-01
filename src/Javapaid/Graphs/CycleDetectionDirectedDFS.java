0d2b6671b2a403b05fccf293b6d2ef682bd9de43
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-10-02 01:08:14 +0530

    Graphs -CycleDetectionDirectedDFS

--- src/Javapaid/Graphs/CycleDetectionDirectedDFS.java
+++ src/Javapaid/Graphs/CycleDetectionDirectedDFS.java
@@ -9,13 +9,51 @@ public class CycleDetectionDirectedDFS extends MyGraph{
         }
 
         //0-vertex
-        graph[0].add(new Edge(0,1,1));
         graph[0].add(new Edge(0,2,1));
 
+        graph[1].add(new Edge(1,0,1));
+
+        graph[2].add(new Edge(2,3,1));
+
+        graph[3].add(new Edge(3,0,1));
+
     }
+
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+        boolean [] vis = new boolean[graph.length];
+        boolean [] stack =  new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]){
+                if (isCycleUtil(graph, i, vis, stack)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isCycleUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis, boolean[] stack) {
+        vis[curr] = true;
+        stack[curr] =true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (stack[e.dest]){
+                return true;
+            }
+            if (!vis[e.dest]&&isCycleUtil(graph,e.dest,vis, stack)){
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
+
     public static void main(String[] args) {
         int v = 7;
         ArrayList<Edge>[] graph = new ArrayList[v]; //null
         createGraph(graph);
+        System.out.println(isCycle(graph));
     }
 }
