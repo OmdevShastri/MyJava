51a15e2ffdecd47ed2b6c3ae9bf762c5be85c12d
Omdev Shastri <omdevshastriwork@gmail.com>
2023-11-27 23:38:32 +0530

    Leet Code problems: Jump Game 1 jump Game 2 with greedy

---
+++ src/LeetCode/JumpGame.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class JumpGame {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+
+        public boolean canJump(int[] nums) {
+            int goal = nums.length - 1;
+            for (int i = nums.length - 2; i >= 0; i--) {
+                if (nums[i] + i >= goal) {
+                    goal = i;
+                }
+            }
+            return goal == 0;
+        }
+    }
+
+}
---
+++ src/LeetCode/JumpGame2.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class JumpGame2 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+
+        public int jump(int[] nums) {
+            int res=0, r=0, l=0,fur =0;
+
+            while (r< nums.length-1){
+                fur =0;
+                for (int i = l; i <= r; i++) {
+                    fur = Math.max(fur, i+nums[i]);
+                }
+                l = r+1;
+                r= fur;
+                res++;
+            }
+            return res;
+        }
+    }
+}
