c4d9c00fc0b775b0c2498978d915ff4f4d9db965
Omdev Shastri <omdevshastriwork@gmail.com>
2023-12-13 23:45:50 +0530

    Leet Code problems: CombinationSumII

--- src/LeetCode/CombinationSumII.java
+++ src/LeetCode/CombinationSumII.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSumII {
@@ -7,8 +9,32 @@ public class CombinationSumII {
 
     }
     class Solution {
-        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> ls = new ArrayList<Integer>();
+        comb(candidates, target, ans, ls, 0);
+        return ans;
+    }
 
+    public void comb(
+            int[] candidates,
+            int target,
+            List<List<Integer>> ans,
+            List<Integer> ls,
+            int index
+    ) {
+        if (target == 0) {
+            ans.add(new ArrayList(ls));
+        } else if (target < 0) return;
+        else {
+            for (int i = index; i < candidates.length; i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+                ls.add(candidates[i]);
+                comb(candidates, target - candidates[i], ans, ls, i + 1);
+                ls.remove(ls.get(ls.size() - 1));
+            }
         }
+    }
     }
 }
