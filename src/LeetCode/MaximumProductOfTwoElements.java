416b9700cedbfb6f2bd80c885fc769cf84b11200
Omdev Shastri <omdevshastriwork@gmail.com>
2023-12-13 14:23:32 +0530

    Leet Code problems: MaximumProductOfTwoElements

--- src/LeetCode/MaximumProductOfTwoElements.java
+++ src/LeetCode/MaximumProductOfTwoElements.java
@@ -1,5 +1,31 @@
 package LeetCode;
 
-public class MaximumProductOfTwoElements {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+public class MaximumProductOfTwoElements {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+//        public int maxProduct(int[] nums) {
+//            if (nums.length==1){
+//                return nums[0];
+//            } else if (nums.length==0) {
+//                return 0;
+//            }
+//            Queue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+//            for (int i :
+//                    nums) {
+//                q.add(i);
+//            }
+//            return (q.poll()-1)*(q.poll()-1);
+//        }
+        public int maxProduct(int[] nums) {
+            Arrays.sort(nums);
+            return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        }
+    }
 }
